135686ce0b32dfe9512085a3fb724d87eb84e921
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2012-12-14 10:50:14 +0000

    Tweaking style

--- datatypes.py
+++ datatypes.py
@@ -6,7 +6,7 @@ class Object(object):
         for key, val in self.contents.items():
             recurse = val.html('{model}.{key}'.format(**locals()))
             out += """
-            <div class="well well-small" ng-class="{{error: form_{model}.$invalid}}">
+            <div class="sub-object" ng-class="{{error: form_{model}.$invalid}}">
                 <label>{key}:</label>
                 {recurse}
             </div>""".format(**locals())
@@ -27,16 +27,20 @@ class List(object):
     def html(self, model):
         name = model.split('.')[-1]
         recurse = self.contents.html('element')
+        if type(self.contents) == Object:
+            style = "list-element-simple"
+        else:
+            style = "list-element"
         return """
-    <div class=list>
-        <div class="" ng-repeat="element in {model}">
-            <button class="btn btn-danger btn-form" ng-click="remove({model},element)"><i class="icon-remove icon-white"></i></button>
-            <ng-form class="form-inline" name="form_element">
-                {recurse}
-            </ng-form>
-        </div>
-        <button class="btn btn-success btn-form" ng-click=""><i class="icon-plus icon-white"></i></button>
-    </div>
+            <div class="list">
+                <div class="{style}" ng-repeat="element in {model}">
+                    <button class="list-btn-remove" ng-click="remove({model},element)"><i class="icon-remove icon-white"></i></button>
+                    <ng-form name="form_element">
+                        {recurse}
+                    </ng-form>
+                </div>
+                <button class="list-btn-add" ng-click=""><i class="icon-plus icon-white"></i></button>
+            </div>
     """.format(**locals())
 
 class Boolean(object):
