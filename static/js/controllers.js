510713e9e891c8a635f9924e6f361d506af62c23
Ian Wright <ian.wright@well.ox.ac.uk>
2013-01-24 16:03:35 +0000

    start of image uploading

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -66,4 +66,60 @@ pycrud.controller("EditContactPersonCtrl", function($scope, $routeParams, $locat
         $scope.person.affiliations.splice($scope.person.affiliations.indexOf(element), 1);
         $scope.form_person.$dirty=true;
     }
+    
+    //============== DRAG & DROP =============
+
+    $scope.setFiles = function(element) {
+    $scope.$apply(function(scope) {
+      console.log('files:', element.files);
+      // Turn the FileList object into an Array
+        $scope.files = []
+        for (var i = 0; i < element.files.length; i++) {
+          $scope.files.push(element.files[i])
+        }
+      $scope.progressVisible = false
+      });
+    };
+
+    $scope.uploadFile = function() {
+        var fd = new FormData()
+        for (var i in $scope.files) {
+            fd.append("uploadedFile", $scope.files[i])
+        }
+        fd.append("person", $scope.person._id)
+        var xhr = new XMLHttpRequest()
+        xhr.upload.addEventListener("progress", uploadProgress, false)
+        xhr.addEventListener("load", uploadComplete, false)
+        xhr.addEventListener("error", uploadFailed, false)
+        xhr.addEventListener("abort", uploadCanceled, false)
+        xhr.open("POST", "/fileupload")
+        $scope.progressVisible = true
+        xhr.send(fd)
+    }
+
+    function uploadProgress(evt) {
+        $scope.$apply(function(){
+            if (evt.lengthComputable) {
+                $scope.progress = Math.round(evt.loaded * 100 / evt.total)
+            } else {
+                $scope.progress = 'unable to compute'
+            }
+        })
+    }
+
+    function uploadComplete(evt) {
+        /* This event is raised when the server send back a response */
+        alert(evt.target.responseText)
+    }
+
+    function uploadFailed(evt) {
+        alert("There was an error attempting to upload the file.")
+    }
+
+    function uploadCanceled(evt) {
+        $scope.$apply(function(){
+            $scope.progressVisible = false
+        })
+        alert("The upload has been canceled by the user or the browser dropped the connection.")
+    }
 });
